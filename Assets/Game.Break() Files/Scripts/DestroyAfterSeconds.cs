b6e71bbe2627d551e05dc1152d34f7156c1e49d6
mariustanase92 <60538321+mariustanase92@users.noreply.github.com>
2021-10-11 12:10:46 +0300

    Playable Demo
    Playable Demo

---
+++ Assets/Game.Break() Files/Scripts/DestroyAfterSeconds.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAfterSeconds : MonoBehaviour {
+
+    public float WaitTillDestroy = 0.5f;
+
+	void Start () {
+        Destroy(gameObject, WaitTillDestroy);
+	}
+}
